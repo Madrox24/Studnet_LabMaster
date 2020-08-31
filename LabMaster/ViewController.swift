05fcf51834c776511eb6535bc47c9022e556f8d4
Robert <rmoryson@man.poznan.pl>
2020-08-31 14:44:22 +0200

    Init

---
+++ LabMaster/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LabMaster
+//
+//  Created by Robert Moryson on 23/08/2020.
+//  Copyright © 2020 Robert Moryson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
